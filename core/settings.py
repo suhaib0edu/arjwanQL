2052146fc95452409f82c723130876900b253b30
Suhaib Eltayeb <suhab.edu@gmail.com>
2023-03-03 00:43:42 +0200

    Django==4.1.7

--- core/settings.py
+++ core/settings.py
@@ -145,7 +145,8 @@ AUTHENTICATION_BACKENDS = [
 GRAPHQL_JWT = {
     "JWT_VERIFY_EXPIRATION": True,
     "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
- 
+    "JWT_EXPIRATION_DELTA": timedelta(minutes=5),
+    "JWT_REFRESH_EXPIRATION_DELTA": timedelta(days=7),
     "JWT_ALLOW_ANY_CLASSES": [
         "graphql_auth.mutations.Register",
         "graphql_auth.mutations.VerifyAccount",
